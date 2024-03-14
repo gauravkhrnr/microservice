5d09b1f68843c26f34ed6fffd29bc9cf188dc9be
gauravkhrnr <gauravkhrnr@gmail.com>
2024-03-14 22:52:03 +0530

    added new service commit

--- currency-exchange-service/src/main/java/com/demo/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/demo/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,10 +2,10 @@ package com.demo.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity  
 @Table(name="Exchange_Value")  
